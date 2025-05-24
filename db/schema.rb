2ddaab52a6c481983a60358fa4951b08bb67a4c9
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-24 16:07:03 +0000

    Reviewing Form Builder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_13_202311) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_24_160114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_13_202311) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "solid_cable_messages", force: :cascade do |t|
